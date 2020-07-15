04070a7247dc3c187fb8f110cc6bb1ec033bd668
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-15 21:38:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(deli)
     if deli.length == 0
       puts "The line is currently empty."
     else 
-      new_line = "The line is currently: "
+      new_line = "The line is currently:"
       deli.each.with_index(1) do |name, i|
         new_line << " #{i}. #{name}"
     end
